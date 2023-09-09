b47ee405da12f099f0dc44f51c6833ca4dc4c3c0
Sameh <samxxhaider@gmail.com>
2023-09-09 18:41:08 +0300

    style(frontend): minor changes to some styles

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,7 +17,7 @@ const RootLayout = ( { children, session } ) => {
         <body className='relative flex min-h-screen flex-col'>  
             <div className='main'>
                 {/*<div className="absolute w-full z-[3] h-full top-0 bg-gradient-to-tr from-indigo-400 to-blue-400 opacity-[0.2] rounded-md"/>*/}
-                <div className='bg_radial-blur'></div>
+                {<div className='bg_radial-blur'></div>}
                 <div className="bg_grid"/>
             </div>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
